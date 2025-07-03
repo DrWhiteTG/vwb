42b4afdf0fdcbf065fb200ff850f3e2d7ebe91b6
DrWhite <68634413+DrWhiteTG@users.noreply.github.com>
2025-07-03 14:01:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from bot import Bot
 import pyrogram.utils
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from MrWhite'
 
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647  #Add Db Channel id Here -10000000000
 
