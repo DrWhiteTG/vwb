4ca4be32b23e8762b69c1535e6c697ef6e9e82de
DrWhite <68634413+DrWhiteTG@users.noreply.github.com>
2025-07-03 17:08:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello from MrWhite'
 
-pyrogram.utils.MIN_CHANNEL_ID = -1009147483647  #Add Db Channel id Here -10000000000
+pyrogram.utils.MIN_CHANNEL_ID = -1002816848112  #Add Db Channel id Here -10000000000
 
 if __name__ == "__main__":
     Bot().run()
